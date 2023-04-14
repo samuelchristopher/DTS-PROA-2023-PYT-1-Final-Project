283b82232b561863be4db07bff1079c613512bb8
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-04-14 20:09:26 +0700

    feat: add cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import models
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
@@ -17,6 +18,20 @@ def get_session():
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
